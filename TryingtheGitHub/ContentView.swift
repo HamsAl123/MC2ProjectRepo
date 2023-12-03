0f9fbd68c2996bc8ffe3fc0e48b4ff3b5c5cf451
Hams Alzahrani <hamsmohsen692@gmail.com>
2023-12-03 12:06:55 +0300

    Initial Commit

---
+++ TryingtheGitHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TryingtheGitHub
+//
+//  Created by Hams Alzahrani on 19/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TryingtheGitHub/TryingtheGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryingtheGitHubApp.swift
+//  TryingtheGitHub
+//
+//  Created by Hams Alzahrani on 19/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TryingtheGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
